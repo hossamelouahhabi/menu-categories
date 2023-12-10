85444b289e620a658b9df36d49c578b47ef9b771
hossam el ouahhabi <hossamelouahhabi@gmail.com>
2023-12-10 17:37:10 +0100

    Making The Category Buttons In A Manual Approach

--- src/App.js
+++ src/App.js
@@ -7,14 +7,49 @@ import Menu from './components/Menu';
 function App() {
   const [menuItems, setManuItems] = useState(menu)
   const [categories, setCategories] = useState([])
+
+  const handleAllCategories = () => {
+    const newItems = menu.map((item) => {
+        return item;
+    })
+    setManuItems(newItems)
+  }
+
+  const handleBreakfastCategorie = () => {
+    const newItems = menu.filter((item) => {
+      if(item.category == 'breakfast'){
+        return item;
+      }
+    })
+    setManuItems(newItems)
+ }
+
+  const handleLunchCategorie = () => {
+    const newItems = menu.filter((item) => {
+      if(item.category == 'lunch'){
+        return item;
+      }
+    })
+    setManuItems(newItems)
+ }
+
+  const handleShakesCategorie = () => {
+    const newItems = menu.filter((item) => {
+      if(item.category == 'shakes'){
+        return item;
+      }
+    })
+    setManuItems(newItems)
+ }
+
   return (
     <main>
-      <section className='manu section'>
+      <section className='menu section'>
         <div className='title'>
           <h2>our menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories />
+        <Categories all={handleAllCategories} breakfast={handleBreakfastCategorie} lunch={handleLunchCategorie} shakes={handleShakesCategorie}/>
         <Menu items={menuItems} />
       </section>
     </main>
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,11 @@
-export default function Categories() {
+export default function Categories({all, breakfast, lunch, shakes}) {
+    
     return(
-        <>
-        
-        </>
+        <div className="btn-container">
+            <button className="filter-btn" onClick={all}>All</button>
+            <button className="filter-btn" onClick={breakfast}>Breakfast</button>
+            <button className="filter-btn" onClick={lunch}>Lunch</button>
+            <button className="filter-btn" onClick={shakes}>Shakes</button>
+        </div>
     )
 }
